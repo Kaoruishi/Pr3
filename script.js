e1b860863e3858f7b017f3170b5e5cdba8a72591
Kaoruishi <glebsefer95@gmail.com>
2025-04-24 12:30:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("openModal").onclick = function() {
+    document.getElementById("modal").style.display = "block";
+}
+
+document.getElementById("closeModal").onclick = function() {
+    document.getElementById("modal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById("modal")) {
+        document.getElementById("modal").style.display = "none";
+    }
+}
+
+function openTab(event, tabName) {
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    var tablinks = document.getElementsByClassName("tablink");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+document.querySelector('.tablink').click();
